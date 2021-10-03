388d2f77097f97afae5164f107fb51e0aa140857
ThanhTran <tranvanthanhtran.tt@gmail.com>
2021-10-03 14:33:11 +0700

    add ShipmentService

---
+++ src/main/java/com/codegym/service/shipment/IShipmentService.java
@@ -0,0 +1,8 @@
+package com.codegym.service.shipment;
+
+import com.codegym.model.Shipment;
+import com.codegym.service.IGeneralService;
+
+public interface IShipmentService extends IGeneralService<Shipment> {
+
+}
---
+++ src/main/java/com/codegym/service/shipment/ShipmentService.java
@@ -0,0 +1,36 @@
+package com.codegym.service.shipment;
+
+import com.codegym.dao.shipment.IShipmentDao;
+import com.codegym.dao.shipment.ShipmentDao;
+import com.codegym.model.Shipment;
+
+import java.util.List;
+
+public class ShipmentService implements IShipmentService {
+    private IShipmentDao shipmentDao = new ShipmentDao();
+
+    @Override
+    public List<Shipment> getAll() {
+        return shipmentDao.getAll();
+    }
+
+    @Override
+    public boolean save(Shipment shipment) {
+        return false;
+    }
+
+    @Override
+    public boolean update(int id, Shipment shipment) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(int id) {
+        return false;
+    }
+
+    @Override
+    public Shipment findById(int id) {
+        return null;
+    }
+}
