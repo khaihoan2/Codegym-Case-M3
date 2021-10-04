d583d3891b77014ac25b330d63a642c8570fdb35
Phan Cong Chinh <phancongchinhpqa@gmail.com>
2021-10-04 17:07:21 +0700

    ImageDAO crud done

--- src/main/java/com/codegym/dao/image/ImageDAO.java
+++ src/main/java/com/codegym/dao/image/ImageDAO.java
@@ -2,8 +2,10 @@ package com.codegym.dao.image;
 
 import com.codegym.dao.DBConnection;
 import com.codegym.model.Image;
+import com.codegym.model.Product;
 
 import javax.xml.transform.Result;
+import java.awt.image.ImagingOpException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -14,6 +16,16 @@ import java.util.List;
 public class ImageDAO implements IImageDAO{
 
     private static final Connection CONNECTION = DBConnection.getConnection();
+    public static final String SQL_INSERT = "INSERT INTO image (name, src, productID) VALUES (?,?,?)";
+    public static final String SQL_UPDATE = "UPDATE image\n" +
+            "SET name = ?,\n" +
+            "    src = ?,\n" +
+            "    product_id = ?\n" +
+            "WHERE id = ?";
+    public static final String SQL_DELETE = "DELETE\n" +
+            "  FROM image\n" +
+            " WHERE id = ?";
+    public static final String SQL_FIND_BY_ID = "SELECT * FROM image WHERE id = ?";
 
     @Override
     public List<Image> getAll() {
@@ -37,21 +49,65 @@ public class ImageDAO implements IImageDAO{
 
     @Override
     public boolean save(Image image) {
-        return false;
+        boolean isSaved = false;
+        try {
+            PreparedStatement statement = CONNECTION.prepareStatement(SQL_INSERT);
+            statement.setString(1, image.getName());
+            statement.setString(2, image.getSrc());
+            statement.setInt(3, image.getProductId());
+            isSaved = statement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return isSaved;
     }
 
     @Override
     public boolean update(int id, Image image) {
-        return false;
+        boolean isUpdated = false;
+        try {
+            PreparedStatement statement = CONNECTION.prepareStatement(SQL_UPDATE);
+            statement.setString(1, image.getName());
+            statement.setString(2, image.getSrc());
+            statement.setInt(3, image.getProductId());
+            statement.setInt(4, image.getId());
+            isUpdated = statement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return isUpdated;
     }
 
     @Override
     public boolean delete(int id) {
-        return false;
+        boolean isDeleted = false;
+        try {
+            PreparedStatement statement = CONNECTION.prepareStatement(SQL_DELETE);
+            statement.setInt(1, id);
+            isDeleted = statement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return isDeleted;
     }
 
     @Override
-    public Image findById(int id) {
-        return null;
+    public Image findById(int inputId) {
+        Image image = null;
+        try {
+            PreparedStatement statement = CONNECTION.prepareStatement(SQL_FIND_BY_ID);
+            statement.setInt(1,inputId);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String src = resultSet.getString("src");
+                int productId = resultSet.getInt("productId");
+                image = new Image(id, name, src, productId);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return image;
     }
 }
